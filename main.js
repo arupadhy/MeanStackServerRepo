7c4533a22d4ceb880e8aad83bc473ad4ba2ed298
arvind upadhyay <arvind.charlotte@gmail.com>
2016-09-06 20:56:42 -0400

    post data to mongo

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 var express = require('express'),
+    book = require('./models/bookModel'),
+    bodyParser = require('body-parser'),
     app = express(),
     bookRouter = express.Router(),
     mongoose = require('mongoose'),
-    PORT = process.env.port || 3000,
-    book = require('./models/bookModel');
+    PORT = process.env.PORT || 3000;
 
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+//Middleware configuration
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
+var db = mongoose.connect('mongodb://localhost/bookAPI')
+
+//routing configuration
 bookRouter.route('/Books')
           .get(function(req, res) {
               book.find(function(err, books) {
@@ -18,7 +24,8 @@ bookRouter.route('/Books')
               });
           })
           .post(function(req, res) {
-
+              var newBook = new book(req.body);
+              res.json(newBook);
           });
 
 app.use('/api',bookRouter);
